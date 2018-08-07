2e410269313bab9a6031c4223958db6ebadefc9b
wjm <1186619054@qq.com>
2018-08-07 15:26:56 +0800

    歌手列表

---
+++ src/api/singer.js
@@ -0,0 +1,28 @@
+import {
+  commonParams,
+  options
+} from './config'
+import {
+  jsonp
+} from 'common/js/jsonp'
+
+function getSingerList() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    gtk: 1664029744
+  })
+
+  return jsonp(url, data, options)
+}
+
+export {
+  getSingerList
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,20 +1,94 @@
 <template>
-  <div class="page">
-    <h1>这是歌手详情页</h1>
+  <div class="singer">
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import Scroll from 'base/scroll/scroll'
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import { Singer } from 'common/js/singer'
+import ListView from 'base/listview/listview'
+
+const HOT_SINGER_LEN = 10
+const HOT_NAME = '热门'
 export default {
   data() {
-    return {}
+    return {
+      singers: []
+    }
   },
   components: {
-    Scroll
+    ListView
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          this.singers = this._normalizeSinger(res.data.list)
+          console.log(this.singers)
+        }
+      })
+    },
+    _normalizeSinger(list) {
+      console.log('处理数据')
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(
+            new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            })
+          )
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(
+          new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          })
+        )
+      })
+      // list 处理为一个二维数组
+      // 对map数组进行排序
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  },
+  created() {
+    this._getSingerList()
   }
 }
 </script>
 
 <style scoped lang="stylus">
+.singer
+  position fixed
+  top 88px
+  bottom 0px
+  width 100%
 </style>
